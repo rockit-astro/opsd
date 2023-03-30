618ef66598212d8e82a0a710bc1bbd5bd55157a6
Paul Chote <p.chote@warwick.ac.uk>
2023-03-30 17:45:00 +0000

    Fix exception when cam_status returns None.

--- warwick/observatory/operations/actions/superwasp/observe_field_base.py
+++ warwick/observatory/operations/actions/superwasp/observe_field_base.py
@@ -229,7 +229,7 @@ class ObserveFieldBase(TelescopeAction):
             return True
 
         # Exposure has timed out: lets find out why
-        status = cam_status(self.log_name, camera_id).get('state', None)
+        status = cam_status(self.log_name, camera_id)
 
         # Lost communication with camera daemon, this is assumed to be unrecoverable
         # We can return here and let the error be handled when restarting the observation
@@ -238,12 +238,13 @@ class ObserveFieldBase(TelescopeAction):
             return False
 
         # Camera may be idle if the pipeline blocked for too long
-        if status is CameraStatus.Idle:
+        state = status.get('state', None)
+        if state is CameraStatus.Idle:
             log.warning(self.log_name, f'Found idle camera {camera_id}; restarting')
             return False
 
         # Power cycling the camera fixes most other errors
-        log.warning(self.log_name, f'Camera has timed out in state {CameraStatus.label(status)}; power cycling')
+        log.warning(self.log_name, f'Camera has timed out in state {CameraStatus.label(state)}; power cycling')
         cam_cycle_power(self.log_name, camera_id)
 
         time.sleep(5)
